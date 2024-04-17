250cad644f5ad5fab3e5074e1498422d302d7fda
Willbemax <max22042007@yandex.ru>
2024-04-17 18:11:32 +0300

    Плагины

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+
+
 
 module.exports = (env) => {
 	return {
@@ -9,5 +13,9 @@ module.exports = (env) => {
 			filename: '[name].[contenthash].js',
 			clean: true,
 		},
+        plugins: [
+            new HtmlWebpackPlugin( {template: path.resolve(__dirname, 'public', 'index.html')}),
+            new webpack.ProgressPlugin()
+        ]
 	};
 };
